9f57a6be94ce5813001a7e251c31ca6baf0280ea
JuanJo5433 <juanjosegiraldo1001@gmail.com>
2025-06-04 18:45:51 -0500

    feat: project initialized and configured

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div >  <h1 className="text-preset-1 ">
+            Hello world!
+        </h1></div>
+    )
+}
+
+export default HomePage
